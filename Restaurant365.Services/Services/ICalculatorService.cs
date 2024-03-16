14cadb139062999d7b60e14671fda4b0e4dcb87e
Akin Okunrinboye <akinzo@hotmail.com>
2024-03-16 07:07:36 -0400

    Add project files.

---
+++ Restaurant365.Services/Services/ICalculatorService.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant365.Services.Services;
+
+public interface ICalculatorService
+{
+    /// <summary>
+    /// Operates addition on an input string. This function works for integer and decimal numbers. Decimals are converted to Integer whole numbers
+    /// </summary>
+    /// <param name="input">String to be operated on</param>
+    /// <param name="maxConstraint">Maximum number of entries after delimiter split</param>
+    /// <returns>A formula as well as an integer result of the Operation</returns>
+    public (string Formula, int Result) Calculate(string input, int maxConstraint = 2);
+}
---
+++ Restaurant365Calculator/App.cs
@@ -0,0 +1,20 @@
+﻿using Restaurant365.Services.Services;
+
+namespace Restaurant365Calculator;
+
+public class App
+{
+    private readonly ICalculatorService _service;
+    public App(ICalculatorService service) 
+    { 
+        _service = service;
+    }    
+
+    public void Run(string[] args)
+    {
+        Console.Write("Enter Input: ");
+        var input = Console.ReadLine() ?? "";
+        var result =_service.Calculate(input);
+        Console.WriteLine($"{result.Formula} = {result.Result}");
+    }
+}
